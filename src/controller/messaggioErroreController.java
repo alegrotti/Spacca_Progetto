1f66b279ee0cfb2cce6b636c793ee0a7f30c9ee3
Tero27 <leoterontoli@gmail.com>
2023-10-18 17:56:01 +0200

    modiche messaggio errore
    aggiunta fxml, css, controller

---
+++ src/controller/messaggioErroreController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class messaggioErroreController {
+
+    @FXML
+    private Button bottoneOk;
+
+    @FXML
+    private Label testoMessaggioErrore;
+
+    @FXML
+    void MouseOkClicked(MouseEvent event) {
+    	bottoneOk.getScene().getWindow().hide();
+    }
+
+}
