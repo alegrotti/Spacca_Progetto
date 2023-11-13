b9e875cf9fa4877ddc7f79d4e8e2c9137bf531f2
Tero27 <leoterontoli@gmail.com>
2023-11-13 19:08:09 +0100

    sfondo per campo da gioco

--- src/controller/CampoGiocoController.java
+++ src/controller/CampoGiocoController.java
@@ -1,9 +1,63 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import model.Partita;
 
 public class CampoGiocoController {
+
+    @FXML
+    private Button buttonLasciare;
+
+    @FXML
+    private Button buttonRilanciare;
+
+    @FXML
+    private HBox hboxMano;
+
+    @FXML
+    private HBox hboxTavolo;
+
+    @FXML
+    private ImageView iconMano1;
+
+    @FXML
+    private ImageView iconMano2;
+
+    @FXML
+    private ImageView iconMano3;
+
+    @FXML
+    private ImageView iconRetroCarta;
+
+    @FXML
+    private ImageView iconTavolo1;
+
+    @FXML
+    private ImageView iconTavolo2;
+
+    @FXML
+    private ImageView iconTavolo3;
+
+    @FXML
+    private ImageView iconTavolo4;
+
+    @FXML
+    private Pane sfondo;
+    
+    @FXML
+    private Pane sfondoScuro;
+
+    @FXML
+    private Label textCrediti;
+
+    @FXML
+    private Label textMoney;
 	
 	@SuppressWarnings("unused")
 	private Partita partita;
