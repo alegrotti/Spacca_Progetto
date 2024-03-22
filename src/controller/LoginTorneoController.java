50d11d5dd3e97f7ffdc02d91e0c909a8109da048
Tero27 <leoterontoli@gmail.com>
2024-03-22 10:33:43 +0100

    tabellone torneo 1

--- src/controller/LoginTorneoController.java
+++ src/controller/LoginTorneoController.java
@@ -2,6 +2,9 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
@@ -23,7 +26,14 @@ public class LoginTorneoController {
 
     @FXML
     void giocaTorneo(ActionEvent event) {
-
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("/view/TabelloneTorneo.fxml"));
+	        Scene scenaHomepage = new Scene(root);
+	        scenaHomepage.getStylesheets().add("/view/tabellonetorneo.css");
+	        GestoreScene.setScene(scenaHomepage,false," - Login tournament");
+    	}catch(Exception e) {
+    		GestoreScene.messaggioErrore("Errore apertura finestra");
+    	}
     }
 
     @FXML
